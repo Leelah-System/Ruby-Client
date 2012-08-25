e67d437c5f50e066ec25ca5f9701f53fd7fdcf16
Vanessa <vanessa.pasque@gmail.com>
2012-08-25 02:48:07 +0200

    Web Services : - Orders : Add, Update, Delete, GET - Orders -> check params

--- app/controllers/leelah_web_services.rb
+++ app/controllers/leelah_web_services.rb
@@ -1,8 +1,6 @@
 require 'rest_client'
 
 class LeelahWebServices < AuthenticationController
-  # To change this template use File | Settings | File Templates.
-
   ##
   # Catalogue
   ##
@@ -75,7 +73,7 @@ class LeelahWebServices < AuthenticationController
   #
   # @param [String, #token]
   # @param [Int, #id]
-  # @return [Boolean, true]
+  # @return [Bool]
   def self.delete_category(token, id)
     response = RestClient.delete 'http://leelah-system.com:3000/api/' + token + '/catalog/categories/' + id
     result = JSON.parse(response.to_str)
@@ -90,6 +88,10 @@ class LeelahWebServices < AuthenticationController
   #
   # @param [Strings, #token]
   # @return [Object, #products]
+  # Todo Add scope      = [with_stock, without_stock, search_text => text, search_reference => text,
+  # Todo                   search_label => text, search_name => text, search_description => text,
+  # Todo                   price => min_price || max_price ]
+  # Todo Add parameters = [ scope, text , min_price, max_price ]
   def self.get_products(token)
     response = RestClient.get 'http://leelah-system.com:3000/api/' + token + '/catalog/products'
     result = JSON.parse(response.to_str)
@@ -154,7 +156,7 @@ class LeelahWebServices < AuthenticationController
   #
   # @param [String, #token]
   # @param [Int, #id]
-  # @return [Boolean, true]
+  # @return [Bool]
   def self.delete_product(token, id)
     response = RestClient.delete 'http://leelah-system.com:3000/api/' + token + '/catalog/products/' + id
     result =  JSON.parse(response.to_str)
@@ -164,4 +166,89 @@ class LeelahWebServices < AuthenticationController
       raise result["msg"]
     end
   end
+
+  ###
+  ## Orders
+  ###
+
+  # Get all orders
+  #
+  # @param [String, #token]
+  # @return [Object, #orders]
+  # Todo Add scope  = with_status
+  # Todo Add status = [0 => init, 1 => pending, 2 => done]
+  def self.get_orders(token)
+    response = RestClient.get 'http://leelah-system.com:3000/api/' + token + '/orders'
+    result =  JSON.parse(response.to_str)
+    if result["success"]
+      @orders = result["result"]['orders']
+      return @orders
+    else
+      raise result["msg"]
+    end
+  end
+
+  # Get one order
+  #
+  # @param [String, #token]
+  # @param [Int, #id]
+  # @@return [Object, #order]
+  def self.get_order(token, id)
+    response = RestClient.get 'http://leelah-system.com:3000/api/' + token + '/orders/' + id
+    result =  JSON.parse(response.to_str)
+    if result["success"]
+      @order = result["result"]
+      return @order
+    else
+      raise result["msg"]
+    end
+  end
+
+  # Add an order
+  #
+  # @param [String, #token]
+  # @param [Object, #params]
+  # @return [Object, #order]
+  def self.add_order(token, params)
+    response = RestClient.post 'http://leelah-system.com:3000/api/' + token + '/orders', {"order" => params}.to_json, :content_type => :json, :accept => :json
+    result =  JSON.parse(response.to_str)
+    if result["success"]
+      @order = result["result"]
+      return @order
+    else
+      raise result["msg"]
+    end
+  end
+
+  # Update an order
+  #
+  # @param [String, #token]
+  # @param [Object, #params]
+  # @return [Object, #order]
+  def self.update_order(token, id, params)
+    response = RestClient.put 'http://leelah-system.com:3000/api/' + token + '/orders/' + id, {"order" => params}.to_json, :content_type => :json, :accept => :json
+    result =  JSON.parse(response.to_str)
+    if result["success"]
+      @order = result["result"]
+      return @order
+    else
+      raise result["msg"]
+    end
+  end
+
+  # Delete an order
+  #
+  # @param [String, #token]
+  # @param [Int, #id]
+  # @return [Bool]
+  def self.delete_order(token, id)
+    response = RestClient.delete 'http://leelah-system.com:3000/api/' + token + '/orders/' + id
+    result =  JSON.parse(response.to_str)
+    if result["success"]
+      return true
+    else
+      raise result["msg"]
+    end
+  end
+
 end
---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,66 @@
+##
+## Orders Management
+##
+class OrderController < AuthenticationController
+
+  #
+  #
+  #
+  def index
+    begin
+      @orders = LeelahWebServices.get_orders(session[:user]["token"])
+    rescue
+      print $!
+    end
+  end
+
+  #
+  #
+  #
+  def show
+    begin
+      @order = LeelahWebServices.get_order(session[:user]["token"], params[:id])
+    rescue
+      print $!
+    end
+  end
+
+  #
+  #
+  #
+  def create
+    begin
+      if params[:order]
+        infos = params[:order]
+        @order = LeelahWebServices.add_order(session[:user]["token"], infos)
+      end
+    rescue
+      print $!
+    end
+  end
+
+  #
+  #
+  #
+  def update
+    begin
+      if params[:order]
+        infos = params[:order]
+        @order = LeelahWebServices.update_order(session[:user]["token"], params[:id], infos)
+      end
+    rescue
+      print $!
+    end
+  end
+
+  #
+  #
+  #
+  def destroy
+    begin
+      @result = LeelahWebServices.delete_order(session[:user]["token"], params[:id])
+    rescue
+      print $!
+    end
+  end
+end
